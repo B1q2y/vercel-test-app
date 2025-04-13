56d5c28cd589b415da42d354f474c5d84c62808a
B1q2Y <yuya.yoshimoto8888@gmail.com>
2025-04-13 18:29:45 +0900

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+// src/app/page.tsx
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello from Vercel!</h1>
+    </main>
+  );
+}
